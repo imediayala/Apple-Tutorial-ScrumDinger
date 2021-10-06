42619a67db3cc5a4adf95eef357a8e00f22c862f
Daniel Ayala <imediayala@gmail.com>
2021-10-06 12:42:02 +0200

    Initial Commit

---
+++ TutorialScrumDinger/TutorialScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialScrumDingerApp.swift
+//  TutorialScrumDinger
+//
+//  Created by Daniel Ayala on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TutorialScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
